a5c8c317799ba073bab0c1d394de618de9826c11
Ronei Gualberto <ronei.mac.rmg@gmail.com>
2018-01-07 11:13:31 -0400

    Utilizando o interval operator do RXJS

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -6,6 +6,9 @@ import {Oferta} from '../shared/oferta.model';
 
 import {ActivatedRoute} from '@angular/router';
 
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/Rx';
+
 @Component({
   selector: 'app-oferta',
   templateUrl: './oferta.component.html',
@@ -26,9 +29,19 @@ export class OfertaComponent implements OnInit {
       this.oferta = oferta;
     })
 
-   /* this.route.params.subscribe((parametro: any) => {
-      console.log(parametro.id);
-    });*/
-  }
+    /*this.route.params.subscribe((parametro: any) => {
+          console.log(parametro.id);
+        }, (erro: any) => {
+
+        }, () => {
+
+        });
+      }*/
+       let tempo = Observable.interval(2000)
+
+       tempo.subscribe((intervalo: number) => {
+          console.log(intervalo);
+       });
+    }
 
 }
