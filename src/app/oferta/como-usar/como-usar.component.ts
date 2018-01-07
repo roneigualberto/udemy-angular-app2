67dc0ad729fcc67c4fa241a536f928d478f81885
Ronei Gualberto <ronei.mac.rmg@gmail.com>
2018-01-07 10:05:16 -0400

    Acessando parametros da rota pai

--- src/app/oferta/como-usar/como-usar.component.ts
+++ src/app/oferta/como-usar/como-usar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import {ActivatedRoute} from '@angular/router';
+
 @Component({
   selector: 'app-como-usar',
   templateUrl: './como-usar.component.html',
@@ -7,9 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComoUsarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+
+   console.log('Id da rota pai',this.route.parent.snapshot.params['id']);
   }
 
 }
