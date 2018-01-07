7c646c53ea39726e0836a24ec0f9776086898179
Ronei Gualberto <ronei.mac.rmg@gmail.com>
2018-01-07 17:47:21 -0400

    Utilizando subject e switchMap

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -4,7 +4,8 @@ import { Event } from '@angular/router/src/events';
 import {OfertasService} from "../ofertas.service";
 import {Oferta}  from '../shared/oferta.model';
 import { Observable } from 'rxjs/Observable';
-
+import { Subject } from 'rxjs/Subject';
+import   'rxjs/add/operator/switchMap';
 @Component({
   selector: 'app-topo',
   templateUrl: './topo.component.html',
@@ -14,14 +15,23 @@ import { Observable } from 'rxjs/Observable';
 export class TopoComponent implements OnInit {
 
   public ofertas: Observable<Oferta[]>;
+  public subjectPesquisa: Subject<string> = new Subject<string>();
 
   constructor(private ofertasService: OfertasService) { }
 
   ngOnInit() {
+    this.ofertas = this.subjectPesquisa.switchMap((termo: string) => {
+      console.log('requisição http para api', termo);
+      return this.ofertasService.pesquisaOfertas(termo);
+    });
+
+    this.ofertas.subscribe((ofertas: Oferta[]) => {
+        console.log(ofertas);
+    });
   }
 
   public pesquisa(termoDaPesquisa): void {
-      console.log(termoDaPesquisa); 
+     /* console.log(termoDaPesquisa); 
 
      this.ofertas = this.ofertasService.pesquisaOfertas(termoDaPesquisa);
 
@@ -29,7 +39,9 @@ export class TopoComponent implements OnInit {
           console.log(ofertas);
      }, (erro: any) => console.log('Erro status:',erro.status),
       () => console.log('Fluxo finalizado')
-    );
+    );*/
+      console.log('keyup caracter: ', termoDaPesquisa);
+      this.subjectPesquisa.next(termoDaPesquisa);
 
      
           
