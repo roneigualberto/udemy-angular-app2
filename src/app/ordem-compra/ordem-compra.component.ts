b26fe1279aa98da5f4df8f85bc7150b5f3642f28
Ronei Gualberto <ronei.mac.rmg@gmail.com>
2018-01-10 23:05:25 -0400

    Realizando two-way binding do formulario

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdemCompraComponent implements OnInit {
 
+  public endereco: string = '';
+  public numero: string = '';
+  public complemento: string = '';
+  public formaPagamento: string = '';
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  public atualizaEndereco(endereco: string) {
+    this.endereco = endereco;
+  }
+
+  public atualizaNumero(numero: number) {
+    this.numero = numero;
+  }
+
+  public atualizaComplemento(complemento: string) {
+    this.complemento = complemento;
+  }
+
+  public atualizaFormaPagamento(formaPagamento: string) {
+    this.formaPagamento = formaPagamento;
+  }
+
 
 }
