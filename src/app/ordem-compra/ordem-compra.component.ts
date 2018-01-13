d3469af89e858d30b90bf10dbc1a38f2915482f3
Ronei Gualberto <ronei.mac.rmg@gmail.com>
2018-01-13 15:56:20 -0400

    Enviando o pedido via HTTP POST

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -12,6 +12,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class OrdemCompraComponent implements OnInit {
 
 
+  public idPedidoCompra: number;
   public formulario: FormGroup = new FormGroup({
       'endereco': new FormControl(null,[Validators.required, Validators.minLength(3), Validators.maxLength(120) ]),
       'numero': new FormControl(null,[Validators.required, Validators.minLength(1), Validators.maxLength(20)]),
@@ -35,6 +36,16 @@ export class OrdemCompraComponent implements OnInit {
         this.formulario.get('numero').markAsTouched();
         this.formulario.get('formaPagamento').markAsTouched();
     } else {
+
+      let pedido: Pedido = new Pedido(
+        this.formulario.value.endereco,
+        this.formulario.value.numero,
+        this.formulario.value.complemento,
+        this.formulario.value.formaPagamento
+      );
+      this.ordemCompraService.efetivarCompra(pedido).subscribe((idPedido: number) => {
+            this.idPedidoCompra = idPedido;
+      });
       console.log('formulario está valido');
     }
 
