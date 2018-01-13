7402287a0f80246a8a06814dd9d082c79c258def
Ronei Gualberto <ronei.mac.rmg@gmail.com>
2018-01-12 20:59:09 -0400

    Injetando o Http no serviço de Ordem de Compra

--- src/app/ordem-compra.service.ts
+++ src/app/ordem-compra.service.ts
@@ -1,8 +1,13 @@
+import {Injectable} from '@angular/core';
+import {Http} from '@angular/http';
 import { Pedido } from './shared/pedido.model';
 
+@Injectable()
 export class OrdemCompraService {
 
+    constructor(private http: Http) {
 
+    }
     public efetivarCompra(pedido: Pedido): void {
         console.log('Chegamos até aqui', pedido);
     }
