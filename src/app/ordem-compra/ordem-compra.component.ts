00be6bb85eb55dc3aac8360631160f6c1a7de0cc
Ronei Gualberto <ronei.mac.rmg@gmail.com>
2018-01-16 21:21:41 -0400

    Impedindo a confirmação de compra se o carrinho estiver vazio

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -28,40 +28,45 @@ export class OrdemCompraComponent implements OnInit {
     private carrinhoService: CarrinhoService) { }
 
   ngOnInit() {
-   this.itensCarrinho =  this.carrinhoService.exibirItens();
+    this.itensCarrinho = this.carrinhoService.exibirItens();
   }
 
   public confirmarCompra(): void {
 
 
     if (this.formulario.status === 'INVALID') {
-      console.log('formulario está invalido');
+
       this.formulario.get('endereco').markAsTouched();
       this.formulario.get('complemento').markAsTouched();
       this.formulario.get('numero').markAsTouched();
       this.formulario.get('formaPagamento').markAsTouched();
     } else {
 
-      let pedido: Pedido = new Pedido(
-        this.formulario.value.endereco,
-        this.formulario.value.numero,
-        this.formulario.value.complemento,
-        this.formulario.value.formaPagamento
-      );
-      this.ordemCompraService.efetivarCompra(pedido).subscribe((idPedido: number) => {
-        this.idPedidoCompra = idPedido;
-      });
-      console.log('formulario está valido');
+      if (this.carrinhoService.exibirItens().length === 0) {
+        alert('Você não selecionou nenhum item!');
+      } else {
+          let pedido: Pedido = new Pedido(
+            this.formulario.value.endereco,
+            this.formulario.value.numero,
+            this.formulario.value.complemento,
+            this.formulario.value.formaPagamento
+          );
+          this.ordemCompraService.efetivarCompra(pedido).subscribe((idPedido: number) => {
+            this.idPedidoCompra = idPedido;
+          });
+      }
+
+
     }
 
 
   }
 
-  public adicionar(item: ItemCarrinho):void {
+  public adicionar(item: ItemCarrinho): void {
     this.carrinhoService.adicionarQuantidade(item);
   }
 
-  public diminuir(item: ItemCarrinho):void {
+  public diminuir(item: ItemCarrinho): void {
     this.carrinhoService.diminuirQuantidade(item);
   }
 }
